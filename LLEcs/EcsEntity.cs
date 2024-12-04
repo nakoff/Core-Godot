5700b6c8e4094d7071c543da79aa3bbef56bb16c
Anton Shtanakov <shtanakof@yandex.ru>
2024-12-04 11:25:53 +0100

    uids

--- LLEcs/EcsEntity.cs
+++ LLEcs/EcsEntity.cs
@@ -6,56 +6,56 @@ namespace Core.LLEcs;
 [GlobalClass]
 public partial class EcsEntity : Node
 {
-    public ulong Id => GetInstanceId();
+	public ulong Id => GetInstanceId();
 
-    private Dictionary<System.Type, Node> _components = new();
+	private Dictionary<System.Type, Node> _components = new();
 
-    public override void _Ready() {
-        base._Ready();
+	public override void _Ready() {
+		base._Ready();
 
-        foreach (var item in GetChildren())
-            _components.Add(item.GetType(), item);
+		foreach (var item in GetChildren())
+			_components.Add(item.GetType(), item);
 
-        EcsWorld.Instance.AddEntity(this);
-    }
+		EcsWorld.Instance.AddEntity(this);
+	}
 
-    public T GetComponent<T>() where T: Node
-    {
-        if (_components.ContainsKey(typeof(T)))
-            return (T)_components[typeof(T)];
-        return default;
-    }
+	public T GetComponent<T>() where T: Node
+	{
+		if (_components.ContainsKey(typeof(T)))
+			return (T)_components[typeof(T)];
+		return default;
+	}
 
-    public Node GetComponent(System.Type type)
-    {
-        if (_components.ContainsKey(type))
-            return _components[type];
-        return default;
-    }
+	public Node GetComponent(System.Type type)
+	{
+		if (_components.ContainsKey(type))
+			return _components[type];
+		return default;
+	}
 
-    public T AddComponent<T>() where T: Node, new()
-    {
-        if (_components.ContainsKey(typeof(T)))
-            RemoveComponent<T>();
+	public T AddComponent<T>() where T: Node, new()
+	{
+		if (_components.ContainsKey(typeof(T)))
+			RemoveComponent<T>();
 
-        var c = new T();
-        _components.Add(c.GetType(), c);
-        AddChild(c);
-        return c;
-    }
+		var c = new T();
+		_components.Add(c.GetType(), c);
+		AddChild(c);
+		return c;
+	}
 
-    public void RemoveComponent<T>() where T: Node
-    {
-        var type = typeof(T);
-        if (!_components.ContainsKey(type))
-            return;
-        _components[type].QueueFree();
-        _components.Remove(type);
-    }
+	public void RemoveComponent<T>() where T: Node
+	{
+		var type = typeof(T);
+		if (!_components.ContainsKey(type))
+			return;
+		_components[type].QueueFree();
+		_components.Remove(type);
+	}
 
-    public override void _ExitTree()
-    {
-        base._ExitTree();
-        EcsWorld.Instance.RemoveEntity(this);
-    }
+	public override void _ExitTree()
+	{
+		base._ExitTree();
+		EcsWorld.Instance.RemoveEntity(this);
+	}
 }
